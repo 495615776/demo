6f0be9c9bfb55d4c9e98553b28cf0cb3610e7200
洛清波 <qingbo.luo@test.com>
2020-01-03 15:57:44 +0800

    注入jdbc

--- itcast-springboot/src/main/java/cn/itcast/springboot/controller/Hello2Controller.java
+++ itcast-springboot/src/main/java/cn/itcast/springboot/controller/Hello2Controller.java
@@ -1,4 +1,4 @@
-package cn.itcast.springboot;
+package cn.itcast.springboot.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -7,16 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
-@EnableAutoConfiguration //启动自动配置
-public class HelloController {
+@RequestMapping("hello2")
+public class Hello2Controller {
 
     @GetMapping("show")
-    public String test(){
-        return "hello springboot 1";
+    public String test() {
+        return "hello springboot 2";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloController.class,args);
-    }
 }
