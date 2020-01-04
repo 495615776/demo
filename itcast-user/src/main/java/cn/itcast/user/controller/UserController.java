18de557fb842c16498264922bdcdba3d4105a4b9
洛清波 <qingbo.luo@test.com>
2020-01-04 13:32:37 +0800

    user模块

--- itcast-user/src/main/java/cn/itcast/user/controller/UserController.java
+++ itcast-user/src/main/java/cn/itcast/user/controller/UserController.java
@@ -1,15 +1,41 @@
 package cn.itcast.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{id}")
+    @ResponseBody
+    public User queryUserById(@PathVariable("id")Long id){
+        return this.userService.queryUserById(id);
+    }
+
     @GetMapping("hello")
+    @ResponseBody
     public String test(){
         return "hello ssm";
+    }
+
+    @GetMapping("/all")
+    public String toUsers(Model model) {
+        // 查询用户
+        List<User> users = this.userService.queryUserAll();
+        // 放入模型
+        model.addAttribute("users", users);
+        // 返回模板名称（就是classpath:/templates/目录下的html文件名）
+        return "users";
     }
 }
