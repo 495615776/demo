84548cf78e0b08a6f7ca43b1f6c61cd781ad5e88
洛清波 <qingbo.luo@test.com>
2020-01-06 16:27:34 +0800

    eureka注册中心 实例

--- itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
+++ itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
@@ -3,10 +3,12 @@ package cn.itcast.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient // 开启Eureka客户端
 public class ItcastServiceConsumerApplication {
 
     @Bean
