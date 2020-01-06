bea941d81845318b9702fc082a95051b05c18121
洛清波 <qingbo.luo@test.com>
2020-01-06 11:13:07 +0800

    provider 模块

---
+++ itcast-service-provider/src/main/java/cn/itcast/service/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package cn.itcast.service.mapper;
+
+import cn.itcast.service.pojo.User;
+import tk.mybatis.mapper.common.Mapper;
+
+@org.apache.ibatis.annotations.Mapper
+public interface UserMapper extends Mapper<User> {
+}
